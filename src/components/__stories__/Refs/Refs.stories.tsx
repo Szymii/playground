9d9ec5dc9fe94d091064f7a20d2499ec6f952dd3
Szymii <szymimelzer9@gmail.com>
2022-10-25 19:24:57 +0200

    upgraded storybook version and added alias resolving

--- src/components/__stories__/Refs/Refs.stories.tsx
+++ src/components/__stories__/Refs/Refs.stories.tsx
@@ -1,6 +1,6 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { ComponentMeta } from "@storybook/react";
-import { theme } from "src/theme";
+import { theme } from "../../../theme";
 import { Refs } from "./Refs";
 
 export default {
