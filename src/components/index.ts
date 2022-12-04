ad187d43f49128c18e220c2c196622113bba8098
Szymii <szymimelzer9@gmail.com>
2022-12-04 15:16:03 +0100

    chenged components folder structure

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,2 @@
-export { ToggleLightButton, IconButton } from "./_atoms";
-export { Nav } from "./Nav";
-export { ErrorBoundary } from "./ErrorBoundary";
+export { ToggleLightButton, IconButton } from "./atoms";
+export { Nav, ErrorBoundary } from "./molecules";
