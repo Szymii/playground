0232d7433ea59e0f3304e91e1d7d7fb73dc18cb3
Szymii <szymimelzer9@gmail.com>
2023-03-21 18:51:27 +0100

    [front] - upgrade storybook version

--- apps/front/src/helpers/storybook/withQueryClient.tsx
+++ apps/front/src/helpers/storybook/withQueryClient.tsx
@@ -1,19 +1,23 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       suspense: true,
+      retry: false,
+      staleTime: 0,
     },
   },
 });
 
 export const withQueryClient = () => {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  //@ts-ignore
+  // @ts-ignore
   return (Story) => (
     <QueryClientProvider client={queryClient}>
       <Story />
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
     </QueryClientProvider>
   );
 };
