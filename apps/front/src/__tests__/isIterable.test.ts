a5d77441224416d93e6bca30f8293fee7f2ac3a3
Szymii <szymimelzer9@gmail.com>
2023-07-10 17:14:31 +0200

    added test for iterable

---
+++ apps/front/src/__tests__/isIterable.test.ts
@@ -0,0 +1,37 @@
+import { describe, expect, test } from "vitest";
+
+describe("check if an object is iterable", () => {
+  // test("if function is iterable", () => {
+  //   expect(isIterable(console.log)).toBe(false);
+  // });
+
+  test("if array is iterable", () => {
+    expect(isIterable([])).toBe(true);
+  });
+
+  test("if object is iterable", () => {
+    expect(isIterable({})).toBe(false);
+  });
+
+  test("if null is iterable", () => {
+    expect(() => isIterable(null)).toThrowError("Value is null or undefined");
+  });
+
+  test("if number is iterable", () => {
+    expect(isIterable(15)).toBe(false);
+  });
+
+  test("if string is iterable", () => {
+    expect(isIterable("hello")).toBe(true);
+  });
+});
+
+const isIterable = (object: any) => {
+  object = object ?? new Error("Value is null or undefined");
+
+  if (object instanceof Error) throw object;
+
+  return (
+    !!object[Symbol.iterator] && typeof object[Symbol.iterator] === "function"
+  );
+};
